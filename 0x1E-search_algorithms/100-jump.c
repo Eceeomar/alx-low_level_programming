2a8f9a1349becddc7c9e3a5a286d98eb04a3ef74
Eceeomar <isiomaenuonye@gmail.com>
2023-04-14 07:58:33 -0700

    fixed the jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,55 +1,44 @@
 #include "search_algos.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
 /**
- * jump_search - Searches for a value in a sorted array of integers using the Jump search algorithm
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in array
- * @value: Value to search for
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
  *
- * Return: First index where value is located, or -1 if value is not present in array or if array is NULL
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int step, prev = 0;
-	int i;
+	int index, m, k, prev;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
-	step = (int)sqrt(size);
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
 
-	while (array[min(step, size) - 1] < value)
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
 	{
-		prev = step;
-		step += (int)sqrt(size);
-		if (prev >= size)
-			return (-1);
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
 	}
-
-	while (array[prev] < value)
-	{
-		prev++;
-		if (prev == min(step, size))
-			return (-1);
-	}
-
-	if (array[prev] == value)
-		return (prev);
 
 	return (-1);
-}
-
-/**
- * min - Computes the minimum of two integers
- * @a: First integer
- * @b: Second integer
- *
- * Return: Minimum of two integers
- */
-int min(int a, int b)
-{
-	return (a < b ? a : b);
 }
